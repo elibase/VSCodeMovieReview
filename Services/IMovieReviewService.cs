807e3d21fe25bbb68ee5b0466949743c7f6976b0
Eli <elibask@ayahoo.ca>
2024-02-10 18:47:12 -0500

    movie review blazor app

---
+++ Services/IMovieReviewService.cs
@@ -0,0 +1,10 @@
+﻿﻿using VSCodeMovieReview.Models;
+
+namespace VSCodeMovieReview.Services
+{
+    public interface IMovieReviewService
+    {
+        List<MovieModel>? GetMovies();
+        MovieModel? GetMovieById(int id);
+    }
+}
